e04b675eb05ec13e1b1cd331ff116fd1d920d26f
Leonel Lima <mml_leo@hotmail.com>
2023-07-24 16:32:35 +0100

    Add spotify-tui config into setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 ln -s ~/dotfiles/hypr ~/.config/hypr
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/waybar ~/.config/waybar
+ln -s ~/dotfiles/spotify-tui ~/.config/spotify-tui
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 chmod u+x ~/dotfiles/hypr/cleanup_after_start.sh
 
