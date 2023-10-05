b66c8e4432c8593e3f4ab9025f8fa001853d8bda
Leonel Lima <mml_leo@hotmail.com>
2023-10-05 01:37:45 +0100

    Add logiops config

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+sudo ln -s ~/dotfiles/logid.cfg /etc/
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
 ln -s ~/dotfiles/hypr ~/.config/hypr
@@ -6,4 +7,3 @@ ln -s ~/dotfiles/waybar ~/.config/waybar
 ln -s ~/dotfiles/spotify-tui ~/.config/spotify-tui
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 chmod u+x ~/dotfiles/hypr/cleanup_after_start.sh
-
