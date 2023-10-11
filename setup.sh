e1f71e63ed18a864de2f395321490cc54ac41723
Leonel Lima <mml_leo@hotmail.com>
2023-10-12 00:30:56 +0100

    Add wofi theme

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
 ln -s ~/dotfiles/hypr ~/.config/hypr
 ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/wofi ~/.config/wofi
 ln -s ~/dotfiles/waybar ~/.config/waybar
 ln -s ~/dotfiles/spotify-tui ~/.config/spotify-tui
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
