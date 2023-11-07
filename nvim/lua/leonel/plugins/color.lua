a6ec82f47bb15517670aa10e1e633191f5515e03
Leonel Lima <mml_leo@hotmail.com>
2023-11-07 00:03:52 +0000

    Clean configuration

--- nvim/lua/leonel/plugins/color.lua
+++ nvim/lua/leonel/plugins/color.lua
@@ -2,10 +2,12 @@ return {
     "catppuccin/nvim", 
     name = "catppuccin", 
     priority = 1000,
-    flavor = "macchiato",
-    term_colors = true,
-    transparent_background = true,
     config = function()
+        require("catppuccin").setup({
+            flavor = "macchiato",
+            term_colors = true,
+            transparent_background = true,
+        })
         vim.cmd.colorscheme "catppuccin"
     end,
 }
--- nvim/lua/leonel/plugins/lsp.lua
+++ nvim/lua/leonel/plugins/lsp.lua
@@ -49,23 +49,6 @@ return {
             }
         })
 
-        local cmp = require('cmp')
-        local cmp_select = {behavior = cmp.SelectBehavior.Select}
-        local cmp_mappings = lsp.defaults.cmp_mappings({
-            ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-            ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-            ['<C-c>'] = cmp.mapping.confirm({ select = true }),
-            ["<C-Space>"] = cmp.mapping.complete(),
-        })
-        -- disable completion with tab
-        -- this helps with copilot setup
-        cmp_mappings['<Tab>'] = nil
-        cmp_mappings['<S-Tab>'] = nil
-
-        lsp.setup_nvim_cmp({
-            mapping = cmp_mappings
-        })
-
         lsp.setup()
 
         vim.diagnostic.config({
