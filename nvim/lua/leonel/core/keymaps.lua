72d6220a3d31592056ef784648d003f8535ab441
Leonel Lima <mml_leo@hotmail.com>
2023-11-17 10:14:07 +0000

    Remove conflicting keymap

--- nvim/lua/leonel/core/keymaps.lua
+++ nvim/lua/leonel/core/keymaps.lua
@@ -29,7 +29,6 @@ vim.keymap.set('n', '<C-Up>', '<cmd>resize -2<CR>')
 vim.keymap.set('n', '<C-Down>', '<cmd>resize +2<CR>')
 vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<CR>')
 vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<CR>')
-vim.keymap.set('n', '<leader>q', '<cmd>q<CR>') -- quit window
 
 -- Switch between window
 vim.keymap.set('n', '<C-h>', '<C-w>h')
