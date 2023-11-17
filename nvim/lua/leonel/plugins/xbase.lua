091d733089fbe6f847954dbadf02fc416789f7df
Leonel Lima <mml_leo@hotmail.com>
2023-11-17 10:12:15 +0000

    Add xbase

---
+++ nvim/lua/leonel/plugins/xbase.lua
@@ -0,0 +1,14 @@
+return {
+    'xbase-lab/xbase',
+    build = 'make install', -- or "make install && make free_space" (not recommended, longer build time)
+    dependencies = {
+        "neovim/nvim-lspconfig",
+        "nvim-telescope/telescope.nvim", -- optional
+        "nvim-lua/plenary.nvim", -- optional/requirement of telescope.nvim
+        "stevearc/dressing.nvim", -- optional (in case you don't use telescope but something else)
+    },
+    config = function()
+        local xbase = require("xbase")
+        xbase.setup({})  -- see default configuration bellow
+    end
+}
