ac1d6562013d93a46e145f78380a22f39b7c1ef6
Leonel Lima <mml_leo@hotmail.com>
2023-12-11 23:12:49 +0000

    Use mason lldb for rust

--- nvim/lua/leonel/plugins/lsp.lua
+++ nvim/lua/leonel/plugins/lsp.lua
@@ -95,8 +95,19 @@ return {
                 -- Next, you can provide targeted overrides for specific servers.
                 ["rust_analyzer"] = function ()
                     local rt = require("rust-tools")
+                    local mason_registry = require("mason-registry")
+
+                    local codelldb = mason_registry.get_package("codelldb")
+                    local extension_path = codelldb:get_install_path() .. "/extension/"
+                    local codelldb_path = extension_path .. "adapter/codelldb"
+                    local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+
                     rt.setup {
+
                         server = {
+                            dap = {
+                                adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+                            },
                             tools = {
                                 hover_actions = {
                                     auto_focus = true,
