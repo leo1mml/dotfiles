b32999e038690198aaa1714b0131cd861eb0eb90
Leonel Lima <mml_leo@hotmail.com>
2024-04-17 10:37:00 +0100

    Update harpoon shortcuts

--- nvim/lua/leonel/plugins/harpoon.lua
+++ nvim/lua/leonel/plugins/harpoon.lua
@@ -10,12 +10,12 @@ return {
         harpoon:extend(extensions.builtins.navigate_with_number());
         -- REQUIRED
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+        vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
 
-        vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<leader>hh", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<leader>ht", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<leader>hn", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<leader>hs", function() harpoon:list():select(4) end)
 
         -- Toggle previous & next buffers stored within Harpoon list
         -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
