afebd2fb4b3ccaaf31259bb34f38d7826780e7f0
Leonel Lima <leonel.lima@loblaw.ca>
2024-05-02 13:01:52 +0100

    Update xbase

--- nvim/lua/leonel/plugins/xbase.lua
+++ nvim/lua/leonel/plugins/xbase.lua
@@ -4,11 +4,31 @@ return {
     dependencies = {
         "neovim/nvim-lspconfig",
         "nvim-telescope/telescope.nvim", -- optional
-        "nvim-lua/plenary.nvim", -- optional/requirement of telescope.nvim
-        "stevearc/dressing.nvim", -- optional (in case you don't use telescope but something else)
     },
     config = function()
         local xbase = require("xbase")
-        xbase.setup({})  -- see default configuration bellow
+        xbase.setup({
+            simctl = {
+                iOS = {
+                    "iPhone 15 Pro", --- only use this devices
+                },
+                watchOS = {},        -- all available devices
+                tvOS = {},           -- all available devices
+            },
+            mappings = {
+                --- Whether xbase mapping should be disabled.
+                enable = true,
+                --- Open build picker. showing targets and configuration.
+                build_picker = "<leader>xb", --- set to 0 to disable
+                --- Open run picker. showing targets, devices and configuration
+                run_picker = "<leader>xr",   --- set to 0 to disable
+                --- Open watch picker. showing run or build, targets, devices and configuration
+                watch_picker = "<leader>xw", --- set to 0 to disable
+                --- A list of all the previous pickers
+                all_picker = "<leader>xp",   --- set to 0 to disable
+                --- vertical toggle log buffer
+                toggle_vsplit_log_buffer = "<leader>lv",
+            },
+        }) -- see default configuration bellow
     end
 }
