b6327ab6060f68503878833735d044529f3e4fd0
Leonel Lima <mml_leo@hotmail.com>
2024-08-12 10:10:43 +0100

    Update neovim config

--- nvim/lua/leonel/core/keymaps.lua
+++ nvim/lua/leonel/core/keymaps.lua
@@ -29,7 +29,3 @@ vim.keymap.set('n', '<C-Up>', '<cmd>resize -2<CR>')
 vim.keymap.set('n', '<C-Down>', '<cmd>resize +2<CR>')
 vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<CR>')
 vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<CR>')
-
--- Switch between window
-vim.keymap.set('n', '<leader>tp', '<cmd>bprev<CR>')
-vim.keymap.set('n', '<leader>tn', '<cmd>bnext<CR>')
--- nvim/lua/leonel/plugins/neorg.lua
+++ nvim/lua/leonel/plugins/neorg.lua
@@ -1,17 +1,21 @@
 return {
     "nvim-neorg/neorg",
     dependencies = { "luarocks.nvim" },
+    lazy = false,
+    verson = "9.0.0",
     config = function()
         require("neorg").setup {
             load = {
                 ["core.defaults"] = {},
                 ["core.concealer"] = {},
+                ["core.ui"] = {},
                 ["core.dirman"] = {
                     config = {
                         workspaces = {
                             notes = "~/notes/personal",
                             work = "~/notes/work",
                         },
+                        default_workspace = "notes",
                     },
                 },
             },
@@ -19,6 +23,10 @@ return {
 
         vim.wo.foldlevel = 99
         vim.wo.conceallevel = 2
+
+        vim.keymap.set("n", "<leader>nt", "<cmd>Neorg journal today<CR>")    -- Open Today's Note
+        vim.keymap.set("n", "<leader>nc", "<cmd>Neorg toggle-concealer<CR>") -- Open Neorg Calendar
+        vim.keymap.set("n", "<leader>nC", "<cmd>Neorg journal custom<CR>")   -- Open Neorg Calendar
     end,
 
 }
