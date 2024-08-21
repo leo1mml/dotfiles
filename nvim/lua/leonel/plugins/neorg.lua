d73b14cc7ba7ed82e3de7ad33217932e6ac413fc
Leonel Lima <llima.smo@eos-uptrade.de>
2024-08-21 17:20:04 +0100

    Add export to neorg

--- nvim/lua/leonel/plugins/neorg.lua
+++ nvim/lua/leonel/plugins/neorg.lua
@@ -11,6 +11,7 @@ return {
             load = {
                 ["core.defaults"] = {},  -- Loads default behaviour
                 ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                ["core.export"] = {},    -- Adds pretty icons to your documents
                 ["core.dirman"] = {      -- Manages Neorg workspaces
                     config = {
                         workspaces = {
