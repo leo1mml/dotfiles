bb32da849eb3712fe0ac1e42ae9f22d1476955ef
Leonel Lima <llima.smo@eos-uptrade.de>
2024-10-08 09:56:29 +0100

    Show file name only lualine

--- nvim/lua/leonel/plugins/lualine.lua
+++ nvim/lua/leonel/plugins/lualine.lua
@@ -14,6 +14,14 @@ return {
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            path = 1,
+          }
+        }
+      }
     }
   end
 }
