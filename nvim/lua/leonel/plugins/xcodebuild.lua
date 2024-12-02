5638723ae2ad0daeb6f69ff69d7d9e27e82812ad
Leonel Lima <llima.smo@eos-uptrade.de>
2024-12-02 13:22:15 +0000

    Add fidget to xcodebuild

--- nvim/lua/leonel/plugins/xcodebuild.lua
+++ nvim/lua/leonel/plugins/xcodebuild.lua
@@ -12,6 +12,42 @@ return {
       code_coverage = {
         enabled = true,
       },
+      integrations = {
+        pymobiledevice = {
+          enabled = true,
+        },
+      },
+      show_build_progress_bar = false,
+      logs = {
+        notify = function(message, severity)
+          local fidget = require("fidget")
+          if progress_handle then
+            progress_handle.message = message
+            if not message:find("Loading") then
+              progress_handle:finish()
+              progress_handle = nil
+              if vim.trim(message) ~= "" then
+                fidget.notify(message, severity)
+              end
+            end
+          else
+            fidget.notify(message, severity)
+          end
+        end,
+        notify_progress = function(message)
+          local progress = require("fidget.progress")
+
+          if progress_handle then
+            progress_handle.title = ""
+            progress_handle.message = message
+          else
+            progress_handle = progress.handle.create({
+              message = message,
+              lsp_client = { name = "xcodebuild.nvim" },
+            })
+          end
+        end,
+      },
     })
 
     -- stylua: ignore start
@@ -32,7 +68,7 @@ return {
     vim.keymap.set("n", "<leader>Xe", "<cmd>XcodebuildTestExplorerToggle<cr>", { desc = "Toggle Test Explorer" })
     vim.keymap.set("n", "<leader>Xs", "<cmd>XcodebuildFailingSnapshots<cr>", { desc = "Show Failing Snapshots" })
 
-    vim.keymap.set("n", "<leader>Xd", "<cmd>XcodebuildSelectDevice<cr>", { desc = "Select Device" })
+    vim.keymap.set("n", "<leader>XD", "<cmd>XcodebuildSelectDevice<cr>", { desc = "Select Device" })
     vim.keymap.set("n", "<leader>Xp", "<cmd>XcodebuildSelectTestPlan<cr>", { desc = "Select Test Plan" })
     vim.keymap.set("n", "<leader>Xq", "<cmd>Telescope quickfix<cr>", { desc = "Show QuickFix List" })
 
