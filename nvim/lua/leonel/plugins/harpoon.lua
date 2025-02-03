fcbb604966052c4671674b517a6adc0bf0d81fe2
Leonel Lima <llima.smo@eos-uptrade.de>
2025-02-03 14:40:21 +0000

    Add descriptions to harpoon

--- nvim/lua/leonel/plugins/harpoon.lua
+++ nvim/lua/leonel/plugins/harpoon.lua
@@ -12,27 +12,39 @@ return {
 
 		vim.keymap.set("n", "<leader>hh", function()
 			harpoon:list():add()
-		end)
+		end, {
+			desc = "Harpoon file",
+		})
 
 		vim.keymap.set("n", "<leader>1", function()
 			harpoon:list():select(1)
-		end)
+		end, {
+			desc = "Go to file 1",
+		})
 		vim.keymap.set("n", "<leader>2", function()
 			harpoon:list():select(2)
-		end)
+		end, {
+			desc = "Go to file 2",
+		})
 		vim.keymap.set("n", "<leader>3", function()
 			harpoon:list():select(3)
-		end)
+		end, {
+			desc = "Go to file 3",
+		})
 		vim.keymap.set("n", "<leader>4", function()
 			harpoon:list():select(4)
-		end)
+		end, {
+			desc = "Go to file 4",
+		})
 
 		-- Toggle previous & next buffers stored within Harpoon list
 		-- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
 		-- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
 		vim.keymap.set("n", "<leader>H", function()
 			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end)
+		end, {
+			desc = "Go to harpoon menu",
+		})
 
 		-- basic telescope configuration
 		-- local conf = require("telescope.config").values
