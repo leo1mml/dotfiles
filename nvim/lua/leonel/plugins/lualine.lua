3b9e739d0d394e8743504b768e78d613e38c51f4
Leonel Lima <llima.smo@eos-uptrade.de>
2025-02-24 18:22:33 +0000

    Show recording messages

--- nvim/lua/leonel/plugins/lualine.lua
+++ nvim/lua/leonel/plugins/lualine.lua
@@ -1,27 +1,34 @@
 -- Set lualine as statusline
 -- See `:help lualine.txt`
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-    lazy = true
-  },
-  config = function()
-    require('lualine').setup {
-      options = {
-        icons_enabled = true,
-        theme = 'tokyonight',
-        component_separators = '|',
-        section_separators = '',
-      },
-      sections = {
-        lualine_c = {
-          {
-            'filename',
-            path = 1,
-          }
-        }
-      }
-    }
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		lazy = true,
+	},
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "tokyonight",
+				component_separators = "|",
+				section_separators = "",
+			},
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+			},
+		})
+	end,
 }
