31b24b1d27e18460659ce09e250876438aeb89cc
Leonel Lima <llima.smo@eos-uptrade.de>
2025-04-23 15:03:12 +0100

    Add format diffs only

--- nvim/lua/leonel/plugins/conform.lua
+++ nvim/lua/leonel/plugins/conform.lua
@@ -5,9 +5,10 @@ return {
   keys = {
     {
       -- Customize or remove this keymap to your liking
-      "<leader>f",
+      "<leader>fb",
       function()
         require("conform").format({ async = true, lsp_fallback = true })
+        vim.notify 'Formatted buffer'
       end,
       mode = "",
       desc = "Format buffer",
@@ -41,6 +42,14 @@ return {
         end
       end,
       desc = 'Toggle autoformat globally',
+    },
+    {
+      '<leader>fd',
+      function()
+        vim.cmd 'DiffFormat'
+        vim.notify 'Formatted diffs'
+      end,
+      desc = 'Format diffs',
     },
   },
   -- Everything in opts will be passed to setup()
@@ -104,5 +113,34 @@ return {
     end, {
       desc = 'Re-enable autoformat-on-save',
     })
+
+    vim.api.nvim_create_user_command('DiffFormat', function()
+      local lines = vim.fn.system('git diff --unified=0'):gmatch('[^\n\r]+')
+      local ranges = {}
+      for line in lines do
+        if line:find('^@@') then
+          local line_nums = line:match('%+.- ')
+          if line_nums:find(',') then
+            local _, _, first, second = line_nums:find('(%d+),(%d+)')
+            table.insert(ranges, {
+              start = { tonumber(first), 0 },
+              ['end'] = { tonumber(first) + tonumber(second), 0 },
+            })
+          else
+            local first = tonumber(line_nums:match('%d+'))
+            table.insert(ranges, {
+              start = { first, 0 },
+              ['end'] = { first + 1, 0 },
+            })
+          end
+        end
+      end
+      local format = require('conform').format
+      for _, range in pairs(ranges) do
+        format {
+          range = range,
+        }
+      end
+    end, { desc = 'Format changed lines' })
   end,
 }
