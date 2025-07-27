cac7e364d7d21926bf43b5f61575d0daf5f01fdd
Leonel Lima <mml_leo@hotmail.com>
2025-07-28 00:00:47 +0100

    Update keymaps

--- nvim/lua/leonel/core/keymaps.lua
+++ nvim/lua/leonel/core/keymaps.lua
@@ -22,6 +22,14 @@ vim.keymap.set("n", "gd", "<C-]>", { noremap = true, silent = true })
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+vim.diagnostic.config({
+  virtual_text = {
+    spacing = 4,
+    source = true,
+  },
+  signs = true,
+  underline = true,
+})
 
 -- Window resizing
 vim.keymap.set('n', '<C-Up>', '<cmd>resize -2<CR>')
