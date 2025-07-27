747be823f1e0dd7235571a5f9755649a03dd1044
Leonel Lima <mml_leo@hotmail.com>
2025-07-27 16:27:07 +0100

    Remap definition

--- nvim/lua/leonel/core/keymaps.lua
+++ nvim/lua/leonel/core/keymaps.lua
@@ -15,6 +15,8 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set('n', "<leader>rr", "<cmd>Ex<CR>")
 
+-- Default remapping
+vim.keymap.set("n", "gd", "<C-]>", { noremap = true, silent = true })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
