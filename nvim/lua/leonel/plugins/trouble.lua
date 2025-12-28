5beb1d07a04ae730468c3d68f1516e2f40343427
Leonel Lima <mml_leo@hotmail.com>
2025-12-28 23:03:59 +0000

    Update trouble

--- nvim/lua/leonel/plugins/trouble.lua
+++ nvim/lua/leonel/plugins/trouble.lua
@@ -3,7 +3,36 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   event = { "BufReadPre", "BufNewFile" },
   keys = {
-    { "<leader>tt", "<cmd>TroubleToggle quickfix<cr>", { desc = "Open a quickfix" } },
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
   },
 
   opts = {},
