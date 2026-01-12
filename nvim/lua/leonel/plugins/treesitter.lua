fbd7f4b97ae51a138db27f1f67a19450a96a5a56
Leonel Lima <mml_leo@hotmail.com>
2026-01-12 00:07:13 +0000

    add norg

--- nvim/lua/leonel/plugins/treesitter.lua
+++ nvim/lua/leonel/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require 'nvim-treesitter.configs'.setup {
       -- A list of parser names, or "all"
-      ensure_installed = { "vimdoc", "javascript", "typescript", "c", "c_sharp", "lua", "rust", "css", "html", "latex", "scss", "svelte", "tsx", "typst", "vue", "regex" },
+      ensure_installed = { "vimdoc", "norg", "javascript", "typescript", "c", "c_sharp", "lua", "rust", "css", "html", "latex", "scss", "svelte", "tsx", "typst", "vue", "regex" },
 
       -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
